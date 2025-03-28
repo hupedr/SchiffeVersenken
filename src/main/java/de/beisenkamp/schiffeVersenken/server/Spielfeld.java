3c448212105667d396298bc24c2a4980fcbb4488
simonbahr06 <simonbahr7@gmail.com>
2025-03-28 18:08:44 +0100

    Änderungen: Überarbeitung der Klassen Schiff und Spielfeld -Liste für Schiffe und Schüsse -setzen von Schiffen -überprüfung von versenken -Schüsse verarbeiten durch Setzen der treffer Arrays und Überprüfung zw. Treffer und Daneben -get Methoden

--- src/main/java/de/beisenkamp/schiffeVersenken/server/Spielfeld.java
+++ src/main/java/de/beisenkamp/schiffeVersenken/server/Spielfeld.java
@@ -1,30 +1,81 @@
 package de.beisenkamp.schiffeVersenken.server;
 
+import nrw.abiturklassen.datenstruktur.linear.List;
+
 public class Spielfeld
 {
-    int[][] feld;
+    List<Schiff> schiffliste;
+    List<String> schussliste;
     public Spielfeld()
     {
-        feld = new int[9][9];
-        for(int i=0;i<feld.length;i++)
-        {
-            for(int j=0;j<feld.length;j++)
-            {
-                feld[i][j]=0;
-            }
-        }
+        schiffliste = new List<>();
+        schussliste = new List<>();
     }
 
-    // KEINE SCHIFF = 0    SCHIFF = 1
+
     // (x/y)   <- Position
 
-    //                             x-Koo ; SEITE BUCHSTABEN
+
     public int schiffPlatzieren(String Position1, String Position2)
     {
         //Koordinaten in Int umwandeln ü
-        int pos1x = (int)Position1.charAt(1);
-        int pos1y = (int)Position1.charAt(0)-64;
-        int pos2x = (int)Position1.charAt(1);
-        int pos2y = (int)Position1.charAt(0)-64;
+        int pos1x = (int)Position1.charAt(0);
+        int pos1y = (int)Position1.charAt(2);
+        int pos2x = (int)Position2.charAt(0);
+        int pos2y = (int)Position2.charAt(2);
+        if(pos1x<pos2x)
+        //1.Koordinate = größte
+        {
+            pos1x = (int)Position2.charAt(0);
+            pos2x = (int)Position1.charAt(0);
+        }
+        if(pos1y<pos2y)
+        {
+             pos1y = (int)Position2.charAt(2);
+             pos2y = (int)Position1.charAt(2);
+        }
+        if(pos1y==pos2y)
+        {
+            schiffliste.append(new Schiff(pos1x, pos1y, pos1x-pos2x+1, true));
+            return 1;
+        }
+        else if(pos1x==pos2x)
+        {
+            schiffliste.append(new Schiff(pos1x, pos1y, pos1y-pos2y+1, false));
+            return 1;
+        }
+        else
+        {
+            return 2;
+        }
+
+    }
+
+    public boolean verarbeiteSchuss(int pX, int pY)
+    {
+        schiffliste.toFirst();
+        schussliste.append(pX+"|"+pY);
+        while(schiffliste.hasAccess())
+        {
+            Schiff schiff = schiffliste.getContent();
+            if(schiff.horizontal)
+            {
+                if(schiff.getX()>=pX && schiff.getX()-(schiff.getSchiffslaenge()-1)<=pX && pY == schiff.getY())
+                {
+                    schiff.treffer(pX, pY);
+                    return true;
+                }
+            }
+            else
+            {
+                if(schiff.getY()>=pY && schiff.getY()-(schiff.getSchiffslaenge()-1)<=pY && pX == schiff.getX())
+                {
+                    schiff.treffer(pX, pY);
+                    return true;
+                }
+            }
+
+        }
+        return false;
     }
 }
